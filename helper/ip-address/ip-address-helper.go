3265b235a8bd95710e75cd64bd81bd71878d17d5
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-06-04 20:43:49 +0700

    feat: create server + basic routing

---
+++ helper/ip-address/ip-address-helper.go
@@ -0,0 +1,46 @@
+package ipaddress
+
+import (
+	"net"
+	"net/http"
+)
+
+func GetServerIP() ([]string, error) {
+	var ips []string
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			// Chỉ lấy IPv4
+			if ip != nil && ip.To4() != nil {
+				ips = append(ips, ip.String())
+			}
+		}
+	}
+	return ips, nil
+}
+
+func GetIP(r *http.Request) string {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.Header.Get("X-Real-Ip")
+	}
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+	return ip
+}
--- main.go
+++ main.go
@@ -3,20 +3,57 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/LostArrows27/snippetbox/env"
+	ipaddress "github.com/LostArrows27/snippetbox/helper/ip-address"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	ip := ipaddress.GetIP(r)
+
+	log.Printf("Request to / : from %s", ip)
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
+func createSnippetHanlder(w http.ResponseWriter, r *http.Request) {
+	ip := ipaddress.GetIP(r)
+	log.Printf("Request to /snippet/create : from %s", ip)
+	w.Write([]byte("Create snippet"))
+}
+
+func viewSnippetHandler(w http.ResponseWriter, r *http.Request) {
+	ip := ipaddress.GetIP(r)
+	log.Printf("Request to /snippet/view : from %s", ip)
+	w.Write([]byte("View snippet"))
+}
+
 func main() {
+	// 1. load ENV
+	env.LoadEnv(".env")
+	port := env.GetEnv("PORT")
+
+	// 1.5. Server IPv4 address
+	ips, err := ipaddress.GetServerIP()
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+	log.Println("Server IPs:", ips[0])
+
+	// 2. configre route
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", viewSnippetHandler)
+	mux.HandleFunc("/snippet/create", createSnippetHanlder)
 
-	port := 4000
+	// 3. run server
 	log.Printf("Starting server on %v", port)
-
-	err := http.ListenAndServe(":4000", mux)
+	err = http.ListenAndServe(":"+port, mux)
 
 	if err != nil {
 		log.Fatal(err)
